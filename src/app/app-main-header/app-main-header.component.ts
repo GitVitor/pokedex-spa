f0d766c2f26a5b892081de4480366f7c938dcf77
Vitor Jose <vitorcosta-pm@hotmail.com>
2024-05-26 15:27:05 -0300

    feat: add app main header component

---
+++ src/app/app-main-header/app-main-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-main-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './app-main-header.component.html',
+  styleUrl: './app-main-header.component.scss'
+})
+export class AppMainHeaderComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppMainHeaderComponent } from './app-main-header/app-main-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppMainHeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'pokedex-spa';
