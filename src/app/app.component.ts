35a8004f3526e3ed6b9763fae3b5f8b8d6073f4d
Vitor Jose <vitorcosta-pm@hotmail.com>
2024-05-26 16:06:20 -0300

    feat: creating mocked list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppMainHeaderComponent } from './app-main-header/app-main-header.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppMainHeaderComponent],
+  imports: [RouterOutlet, AppMainHeaderComponent, PokemonListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'pokedex-spa';
-}
+export class AppComponent {}
